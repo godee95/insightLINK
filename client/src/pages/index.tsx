d61f1604228131835311afeab2fc5d434a96667b
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-07-08 12:16:26 +0900

    fix: 랜딩페이지 회원가입 수정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -26,10 +26,10 @@ export default function Home() {
       <p className="text-2xl">갤러리 속 숨겨진 인사이트를 이어주는 아카이브</p>
       <p className="py-[2rem] font-bold text-8xl">insightLINK</p>
       {!token && (
-        <div className="flex w-[20rem] justify-between py-[4rem]">
-          <div className="flex h-[3.375rem] px-7 justify-center items-center rounded bg-gray-200 cursor-pointer">
+        <div className="flex w-[20rem] justify-center py-[4rem]">
+          {/* <div className="flex h-[3.375rem] px-7 justify-center items-center rounded bg-gray-200 cursor-pointer ">
             <SignupButton />
-          </div>
+          </div> */}
           <LoginBtn />
         </div>
       )}
